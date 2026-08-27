ba847b5881a5ec0b7c9d4dd3421fb70fe0de0c3b
Sravanthi <sravnthikanukula@gmail.com>
2026-08-27 14:33:28 +0530

    Fix email from address

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ echo "Hello from the Jenkins-triggered build!"
 date
 Updated at Mon Aug 24 17:24:23 IST 2026
 Updated at Mon Aug 24 17:35:11 IST 2026 for email notification
+test email fix Thu Aug 27 14:32:59 IST 2026
