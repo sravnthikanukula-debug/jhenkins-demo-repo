aad9d1ef7981efad2974cca5fbea37d57428ecf7
Sravanthi <sravnthikanukula@gmail.com>
2026-08-27 14:48:35 +0530

    Fix email from address

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ date
 Updated at Mon Aug 24 17:24:23 IST 2026
 Updated at Mon Aug 24 17:35:11 IST 2026 for email notification
 test email fix Thu Aug 27 14:32:59 IST 2026
+test email fix Thu Aug 27 14:48:22 IST 2026
